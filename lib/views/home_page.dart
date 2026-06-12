14f8fedd9ecee91b7d5e8246ec144eb471318a49
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2026-06-12 20:26:19 +0700

    Update Code

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:app_berita_roni/views/articles/article_page.dart';
 import 'package:app_berita_roni/views/users/user_aticle_page.dart';
 import 'package:app_berita_roni/views/users/user_page.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../config/session.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,22 +21,53 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       _pilBottomNav = index;
     });
-
   }
+
   static final List<Widget> _halaman = [
     ArticlePage(),
     UserAticlePage(),
-    UserPage()
+    UserPage(),
   ];
+
+  void logout() async {
+    await SessionManager.clearSession();
+    context.push('/login');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Portal Berita",style: TextStyle(color: Colors.white),),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout, color: Colors.white),
+            onPressed: () {
+              AwesomeDialog(
+                context: context,
+                dialogType: DialogType.warning,
+                headerAnimationLoop: false,
+                animType: AnimType.bottomSlide,
+                title: 'Logout',
+                desc: 'Apakah anda yakin ingin keluar?',
+                buttonsTextStyle: const TextStyle(color: Colors.white),
+                showCloseIcon: true,
+                btnCancelOnPress: () {},
+                btnOkOnPress: () {
+                  logout();
+                },
+              ).show();
+            },
+          ),
+        ],
+      ),
       body: Center(child: _halaman[_pilBottomNav]),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _klikBottomNav,
         currentIndex: _pilBottomNav,
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey.shade500,
+        backgroundColor: Colors.black,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.article), label: "Article"),
           BottomNavigationBarItem(icon: Icon(Icons.draw), label: "My Article"),
@@ -42,4 +77,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import rive_native
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  RiveNativePlugin.register(with: registry.registrar(forPlugin: "RiveNativePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
