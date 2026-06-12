8f71df883bc03f8cf7e8f384b766b93faf437f19
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2026-06-12 17:54:55 +0700

    Update Code

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -16,76 +16,87 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 25),
-                  Text(
-                    "Login",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 30),
-                  TextFormField(
-                    controller: username,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.wrap_text, color: Colors.red),
-                      labelText: "Username",
-                      filled: true,
-                      fillColor: Colors.amber.shade100,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.green, Color(0xFF220033)],
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 25),
+                    Text(
+                      "Login",
+                      style: TextStyle(color:Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      controller: username,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.wrap_text, color: Colors.red),
+                        labelText: "Username",
+                        filled: true,
+                        fillColor: Colors.amber.shade100,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
+                      keyboardType: TextInputType.text,
+                      validator: (val) {
+                        return val!.isEmpty ? "Tidak boleh kosong" : null;
+                      },
                     ),
-                    keyboardType: TextInputType.text,
-                    validator: (val) {
-                      return val!.isEmpty ? "Tidak boleh kosong" : null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: password,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.wrap_text, color: Colors.red),
-                      labelText: "Password",
-                      filled: true,
-                      fillColor: Colors.amber.shade100,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide.none,
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: password,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.wrap_text, color: Colors.red),
+                        labelText: "Password",
+                        filled: true,
+                        fillColor: Colors.amber.shade100,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
+                      keyboardType: TextInputType.text,
+                      validator: (val) {
+                        return val!.isEmpty ? "Tidak boleh kosong" : null;
+                      },
                     ),
-                    keyboardType: TextInputType.text,
-                    validator: (val) {
-                      return val!.isEmpty ? "Tidak boleh kosong" : null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 50),
-                      // Full width, height: 50
-                      backgroundColor: Colors.red,
-                      // Change button color
-                      foregroundColor: Colors.white, // Change text color
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 50),
+                        // Full width, height: 50
+                        backgroundColor: Colors.red,
+                        // Change button color
+                        foregroundColor: Colors.white, // Change text color
+                      ),
+                      onPressed: () =>{},
+                      child: Text("Login"),
                     ),
-                    onPressed: () =>{},
-                    child: Text("Login"),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      context.push('/register');
-                    },
-                    child: const Text('Belum punya akun? Register'),
-                  ),
-                ],
+                    TextButton(
+                      onPressed: () {
+                        context.push('/register');
+                      },
+                      child: const Text('Belum punya akun? Register',style: TextStyle(color: Colors.white),),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
